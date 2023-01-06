22555d319097fd7d236e9e2f07a84fbd86a88060
Esra Emine Evren <88592010+esraevren@users.noreply.github.com>
2023-01-06 16:50:53 +0300

    redux installed

---
+++ src/redux/contactSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const contactSlice= createSlice({
+  name:"contacts",
+  initialState:{
+    items:[]
+  },
+  reducers:{}
+})
+
+export default contactSlice.reducer;
